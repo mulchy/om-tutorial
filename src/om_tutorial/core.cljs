d50bf1443239ea85692bd3f17c3623ee9e3fb775
Ryan Mulcahy <ryan.mulcahy@code42.com>
2015-10-16 19:18:47 -0500

    Save some changes

--- src/om_tutorial/core.cljs
+++ src/om_tutorial/core.cljs
@@ -6,3 +6,12 @@
 (enable-console-print!)
 
 (println "Hello world!")
+
+(defui HelloWorld
+  Object
+  (render [this]
+          (dom/div nil "Hello, world!")))
+
+(def hello (om/factory HelloWorld))
+
+(js/ReactDOM.render (hello) (gdom/getElement "app"))
